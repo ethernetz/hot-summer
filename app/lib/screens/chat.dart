b4f023a604f0ba173c7354965011ee163ac5e6d3
Ethan Netz <ethannetz@hotmail.com>
2023-03-07 13:54:19 -0500

    try rebuilding with no suggestions

--- app/lib/screens/chat.dart
+++ app/lib/screens/chat.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ChatScreen extends StatelessWidget {
   const ChatScreen({super.key});
@@ -9,9 +10,20 @@ class ChatScreen extends StatelessWidget {
       body: ListView.builder(
         itemCount: 10,
         itemBuilder: (ctx, index) => Container(
-          padding: EdgeInsets.all(8),
-          child: Text("this works!"),
+          padding: const EdgeInsets.all(8),
+          child: const Text("this works!"),
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {
+          FirebaseFirestore.instance
+              .collection('chats/4K08x0809YgBzdcaed37/messages')
+              .snapshots()
+              .listen((data) {
+            // print(data.docs[0]['text']);
+          });
+        },
       ),
     );
   }
