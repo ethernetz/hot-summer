fe52c36bbaa05b5b5cb976ebda8a0cd83dd89692
Ethan Netz <ethannetz@hotmail.com>
2023-03-29 01:54:47 -0400

    functions formatting

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -4,12 +4,11 @@ import * as admin from 'firebase-admin';
 admin.initializeApp(functions.config().firebase);
 
 export const createUserDocument = functions.auth.user().onCreate((userRecord) => {
-	functions.logger.info('Running createUserDocument!');
+	functions.logger.info(`Running createUserDocument! ${userRecord.uid}`);
 	const user = {
 		uid: userRecord.uid,
 		email: userRecord.email,
 		displayName: userRecord.displayName,
-		isPremium: false,
 	};
-    return admin.firestore().collection('users').doc(userRecord.uid).set(user);
+	return admin.firestore().collection('users').doc(userRecord.uid).set(user);
 });
