24f481ff67a1807d854097a818c8a691ee0a65ab
Ethan Netz <ethannetz@hotmail.com>
2023-03-30 15:27:45 -0400

    update google-services.json

--- firebase/start.sh
+++ firebase/start.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-# Copy node_modules into volume-mounted directory so we can see them in dev environment
-cp -r /home/node/functions-cache/. /home/node/app/functions/. && 
-
 firebase --project=hot-summer-9a82c emulators:start --only auth,firestore,functions --import=./emulator-cache --export-on-exit & 
 
 npm run build:watch --prefix ./functions
